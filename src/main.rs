2828b3f1686e337221f6de46f60e4b7bf3ef9cb9
Robin Avery <robin@ribbanya.dev>
2022-09-14 13:03:43 -0400

    things

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,14 @@ use std::{
 };
 use log::{debug, LevelFilter};
 use unwrap_elf::settings::Settings;
-use object::{Object, ObjectSection, ObjectSymbol, ObjectSymbolTable};
-use object::SymbolKind::Text;
-use ppc750cl::{disasm_iter, Opcode};
+use object::{
+    Object,
+    ObjectSection,
+    ObjectSymbol,
+    ObjectSymbolTable,
+    SymbolKind::Text,
+};
+use ppc750cl::{disasm_iter};
 
 
 fn main() {
@@ -47,32 +52,44 @@ fn main() {
     elf_file_ref
         .and(symbol_table)
         .as_ref()
-        .map(|t| t.symbols())
+        .map(move |t| t.symbols())
         .into_iter()
         .by_ref()
         .flatten()
-        .filter(|s| s.is_definition() && s.size() > 0 && s.kind() == Text)
-        .filter_map(|s| {
-            let index = s.section_index();
+        .filter(move |sym| sym.is_definition() && {
+            if sym.size() > 0 {
+                true
+            } else {
+                debug!("'{}' has no size", sym.name().unwrap_or(&sym.address().to_string()));
+                false
+            }
+        } && sym.kind() == Text)
+        .filter_map(move |sym| {
+            let index = sym.section_index();
             let section = index
                 .and_then(|i| elf_file_ref
                     .unwrap()
                     .section_by_index(i)
                     .ok());
-            let address: u32 = s.address().try_into().unwrap();
+            let address: u32 = sym.address().try_into().unwrap();
 
             section
-                .and_then(|c| c.data_range(address as u64, s.size()).ok())
+                .and_then(|c| c.data_range(address as u64, sym.size()).ok())
                 .flatten()
                 .map(|c| disasm_iter(c, address))
+                .map(|disasm| (sym, disasm))
         })
-        .flatten()
-        .for_each(|ins| {
-            let code = ins.code;
-            let address = ins.addr;
-            let mnemonic = Opcode::detect(code).mnemonic();
-            write!(out_file, "{address:08X} {code:08X} {mnemonic}\n").unwrap();
-            debug!("{}", mnemonic);
+        .for_each(|(sym, disasm)| {
+            if let Ok(name) = sym.name() {
+                write!(out_file, "{name}:\n").unwrap();
+                for ins in disasm {
+                    let code = ins.code;
+                    let address = ins.addr;
+                    let simplified = ins.simplified();
+                    let simplified_str = simplified.to_string();
+                    write!(out_file, "/* {address:08X} {code:08X} */ {simplified_str}\n").unwrap();
+                }
+            }
         });
 
 
